0f1385467c06d0d8c37efa308e0661a634d3a74c
njshofe <njshofe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-25 18:44:10 +0000

    tagging version 1.1.1 - fix for 404 not found error on images
    git-svn-id: https://plugins.svn.wordpress.org/smoothness-slider-shortcode/tags/1.1.1@549070 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ show_image.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'lib/phpthumb/ThumbLib.inc.php';
+
+$filename = '../../' . $_GET['filename'];
+$w = $_GET['w'];
+$h = $_GET['h'];
+
+$path_parts = pathinfo($filename);
+if(!file_exists($path_parts['dirname'] . '/' . $w.'x'.$h.'-'.$path_parts['basename'])){
+	try
+	{
+		$thumb = PhpThumbFactory::create($filename);
+	}
+	catch (Exception $e)
+	{
+		// handle error here however you'd like
+	}
+
+	$thumb->adaptiveResize($w, $h);
+	$thumb->save($path_parts['dirname'] . '/' . $w.'x'.$h.'-'.$path_parts['basename']);
+
+	header('Content-Type: image/' . $path_parts['extension']);
+	readfile($path_parts['dirname'] . '/' . $w.'x'.$h.'-'.$path_parts['basename']);
+}else{
+	header('Content-Type: image/' . $path_parts['extension']);
+	readfile($path_parts['dirname'] . '/' . $w.'x'.$h.'-'.$path_parts['basename']);
+}
+?>
